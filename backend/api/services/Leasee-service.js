5a0463d13483249e9a346006ddc72c1e5462ca69
Akash Bharadwaj Karthik <bharadwajkarthik.a@northeastern.edu>
2022-12-05 03:47:48 -0500

    Adding Leasee services

---
+++ backend/api/services/Leasee-service.js
@@ -0,0 +1,29 @@
+import Leasee from '../models/Leasee.js';
+
+// service to save, search, get, update and remove Leasees to be exported from here  
+
+export const save = (newLeasee) => {
+   const leasee = new Leasee(newLeasee);
+   return leasee.save();
+}
+
+export const search = (query) =>{
+   const params = {...query};
+   return Leasee.find(params)
+}
+
+export const get = (id) =>{
+   const retrievedLeasee = Leasee.findById(id).exec();
+   return retrievedLeasee;
+}
+
+export const update = (leasee) =>{
+   leasee.lastModifiedDate = new Date();
+   const updatedLeasee = Leasee.findByIdAndUpdate(leasee.id, leasee, {new: true}).exec();
+   return updatedLeasee;
+}
+
+export const remove = (id) => {
+   const deletedLeasee = Leasee.findByIdAndDelete(id).exec();
+   return deletedLeasee;
+}
